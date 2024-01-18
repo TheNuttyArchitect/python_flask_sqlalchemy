2c525cf14425d700dc5a46e03acf20f6213ca7fd
Ben Chesnut <bchesnut@activecampaign.com>
2024-01-17 20:24:37 -0500

    Initial functioning app with nothing but a health check endpoint

---
+++ tests/test_app_factory.py
@@ -0,0 +1,12 @@
+from sqlalchemy_tutorial.app_factory import create_app
+
+
+def test_conf():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+
+def test_healthcheck(client):
+    response = client.get('healthcheck')
+    assert response.data == b'alive'
+    
